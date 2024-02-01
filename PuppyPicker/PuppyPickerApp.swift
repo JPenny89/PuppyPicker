a7b51b529a76ccddc9e738f6b15ff75357e3af6c
James Penny <james_penny@hotmail.co.uk>
2024-02-01 12:43:02 +0000

    Initial Commit

---
+++ PuppyPicker/PuppyPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PuppyPickerApp.swift
+//  PuppyPicker
+//
+//  Created by James Penny on 01/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct PuppyPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
